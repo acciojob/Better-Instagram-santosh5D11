faeba586e5ff875df2b3c011f515bd34e9f6f52a
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-16 00:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your code here
+const images = document.querySelectorAll(".image");
+
+let draggedElement = null;
+
+images.forEach((image) => {
+    image.addEventListener("dragstart", function () {
+        draggedElement = this;
+    });
+
+    image.addEventListener("dragover", function (event) {
+        event.preventDefault();
+    });
+
+    image.addEventListener("drop", function (event) {
+        event.preventDefault();
+
+        if (draggedElement === this) {
+            return;
+        }
+
+        const tempId = draggedElement.id;
+
+        draggedElement.id = this.id;
+        this.id = tempId;
+    });
+});
